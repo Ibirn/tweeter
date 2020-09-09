08908f4af6fd821155a6fc48641e71d8ccbe5772
Ibirn <kmcricciuti@gmail.com>
2020-09-09 00:08:01 +0000

    Add helper function for counting characters.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,12 @@
-console.log("I'm loaded, Charlie")
-//monitorEvents(object [, events]).
-$(document).ready(() => {
+//Function to count available characters in tweet:
+$(() => {
   $('textarea').on("keyup", function() {
-    //console.log(140 - this.value.length);
-    $(this).siblings(".counter").text(140 - this.value.length)
-    let chars = (140 - this.value.length)
+    $(this).siblings(".counter").text(140 - this.value.length);
+    let chars = (140 - this.value.length);
     if (chars < 0){
-      $(this).siblings(".counter").addClass("negative")
-
+      $(this).siblings(".counter").addClass("negative");
     } else if (chars >= 0){
-      $(this).siblings(".counter").removeClass("negative")
+      $(this).siblings(".counter").removeClass("negative");
     }
   })
-  
 });
