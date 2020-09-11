d324ee894af69ffd5163967fc204417569e7117b
Ibirn <kmcricciuti@gmail.com>
2020-09-11 14:41:30 +0000

    Linted

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,14 @@
 //Function to count available characters in tweet:
+/* global document */
+/* eslint-env jquery */
 $(() => {
   $('textarea').on("keyup", function() {
     $(this).siblings(".counter").text(140 - this.value.length);
     let chars = (140 - this.value.length);
-    if (chars < 0){
+    if (chars < 0) {
       $(this).siblings(".counter").addClass("negative");
-    } else if (chars >= 0){
+    } else if (chars >= 0) {
       $(this).siblings(".counter").removeClass("negative");
     }
-  })
+  });
 });
